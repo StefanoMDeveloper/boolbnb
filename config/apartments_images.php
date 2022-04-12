77697ed38a42c0cce271be56170690ecf8b8f1f1
AntonyMorgante <antonymorgante1@gmail.com>
2022-04-12 10:24:15 +0200

    first config file

---
+++ config/apartments_images.php
@@ -0,0 +1,379 @@
+<?php
+
+return [
+	[
+		"apartment_id" => 1,
+		"url" => "https://a0.muscache.com/im/pictures/86983639/ec0407b9_original.jpg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 1,
+		"url" => "https://a0.muscache.com/im/pictures/86984277/a7b7ca52_original.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 1,
+		"url" => "https://a0.muscache.com/im/pictures/86983461/7961fcec_original.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 1,
+		"url" => "https://a0.muscache.com/im/pictures/86984094/4a1aa507_original.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 1,
+		"url" => "https://a0.muscache.com/im/pictures/86984586/be197de9_original.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 2,
+		"url" => "https://a0.muscache.com/im/pictures/e620443d-d388-4dc2-b3c9-f300a1d6ba4d.jpg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 2,
+		"url" => "https://a0.muscache.com/im/pictures/0c049379-ba5d-4119-85d1-10954349548c.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 2,
+		"url" => "https://a0.muscache.com/im/pictures/6cc77bbd-4369-4ba7-8ffd-93d056248d91.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 2,
+		"url" => "https://a0.muscache.com/im/pictures/427cce8b-0477-4e17-ac3d-7d21d7043f36.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 2,
+		"url" => "https://a0.muscache.com/im/pictures/ce4982a1-e7a5-4da9-a4b7-6ffe29348631.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 3,
+		"url" => "https://a0.muscache.com/im/pictures/miso/Hosting-597476197966578255/original/98a1b2a9-ebc8-439d-92b8-f461efaccf8e.jpeg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 3,
+		"url" => "https://a0.muscache.com/im/pictures/miso/Hosting-597476197966578255/original/0e1fa2d8-d5f0-4c54-9221-7ded466d29ad.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 3,
+		"url" => "https://a0.muscache.com/im/pictures/miso/Hosting-597476197966578255/original/bfa2130e-ae9b-4ffb-b753-7a828395d281.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 3,
+		"url" => "https://a0.muscache.com/im/pictures/miso/Hosting-597476197966578255/original/6afc95ef-c247-43c0-aa3f-b2214807fd94.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 3,
+		"url" => "https://a0.muscache.com/im/pictures/miso/Hosting-597476197966578255/original/de6af409-e22d-4a6b-a12e-c2b6501e1f9c.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 4,
+		"url" => "https://a0.muscache.com/im/pictures/2eb47340-ed27-4ec9-aec7-454eb21d7958.jpg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 4,
+		"url" => "https://a0.muscache.com/im/pictures/a9e875e2-13e3-4e8f-a41d-6f15433d637b.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 4,
+		"url" => "https://a0.muscache.com/im/pictures/75bf3093-dc21-4bf9-9ad6-aa9ab529b00e.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 4,
+		"url" => "https://a0.muscache.com/im/pictures/17ad21af-bc6d-4376-ab0e-85d1d0fa4430.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 4,
+		"url" => "https://a0.muscache.com/im/pictures/319161d7-a023-4c70-9ee7-0084bd5ec506.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 5,
+		"url" => "https://a0.muscache.com/im/pictures/3b2d6755-e4d5-4224-94b8-c4e43d2268f5.jpg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 5,
+		"url" => "https://a0.muscache.com/im/pictures/miso/Hosting-51448589/original/992dff5b-ff7f-499c-a7f0-a13391583cc1.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 5,
+		"url" => "https://a0.muscache.com/im/pictures/c6eb3a00-422b-4989-a9ed-5ad9a35e5a4c.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 5,
+		"url" => "https://a0.muscache.com/im/pictures/miso/Hosting-51448589/original/608c8a35-fe8b-4be6-87ff-6d1a1b9f39c7.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 5,
+		"url" => "https://a0.muscache.com/im/pictures/57ed6e79-19d8-43b6-87f9-4a24e6e1517c.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 6,
+		"url" => "https://a0.muscache.com/im/pictures/7d1c0941-9b9c-4ff5-81b2-5bcbf35bbdc4.jpg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 6,
+		"url" => "https://a0.muscache.com/im/pictures/b8127a4f-0375-41ae-9958-f84ef09b137a.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 6,
+		"url" => "https://a0.muscache.com/im/pictures/6286924f-5b96-425f-835c-28e728efb9cf.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 6,
+		"url" => "https://a0.muscache.com/im/pictures/070672f0-af62-4a6e-81e4-9d2c684d1bbc.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 6,
+		"url" => "https://a0.muscache.com/im/pictures/414ae013-8db0-40d0-b407-43c81bde43b2.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 7,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-24446953/original/825d1262-0f3b-4638-b5cb-a942b578460c.jpeg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 7,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-24446953/original/f190b1d0-4773-4ce5-a127-d4e0d39dfadf.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 7,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-24446953/original/077e9d10-1274-4469-bca3-76ab709a14bb.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 7,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-24446953/original/b2139ae8-3bde-4935-b9f3-c132b5d8c7f3.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 7,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-24446953/original/c44b6e79-79f0-4e33-aeaf-d92e165b8ed6.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 8,
+		"url" => "https://a0.muscache.com/im/pictures/miso/Hosting-53057923/original/ead8f6a6-b98a-4243-98ec-03ca08730d19.jpeg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 8,
+		"url" => "https://a0.muscache.com/im/pictures/miso/Hosting-53057923/original/77fd08f9-2da0-4c25-be33-2c9c4d34ee89.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 8,
+		"url" => "https://a0.muscache.com/im/pictures/6f1cf48d-2cab-4760-ba82-949c3ffefc11.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 8,
+		"url" => "https://a0.muscache.com/im/pictures/8ec4587d-1933-4311-864a-17d2d5bd89d8.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 8,
+		"url" => "https://a0.muscache.com/im/pictures/miso/Hosting-53057923/original/340725ed-61a6-4342-a791-8c523788afeb.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 9,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-16373405/original/3569baa9-b318-4052-a7dd-885778f03b76.jpeg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 9,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-16373405/original/e19a47b8-780b-4437-bafb-82e3c9c7571a.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 9,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-16373405/original/242e81f9-c305-47a0-a582-1ed912216d2b.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 9,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-16373405/original/71ef510a-194e-4706-9c3f-8b555ea63f5e.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 9,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-16373405/original/ec99e353-f5da-40b3-8a59-69dc4362b614.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 10,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-53194081/original/e6e42eb0-a170-4efc-ae43-7699023975b5.jpeg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 10,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-53194081/original/306abd72-a474-4a62-a80b-77ed4417548c.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 10,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-53194081/original/c8347d80-0b34-4d64-a46e-2fc3556c5ee9.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 10,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-53194081/original/c2c4d5c9-3e00-4f6d-8a1e-5e10bd119dbc.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 10,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-53194081/original/ee9be1bc-f0ac-4dbb-9045-898f3b8ed8f5.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 11,
+		"url" => "https://a0.muscache.com/im/pictures/eec218d5-5b02-433f-a5c3-80185f29705c.jpg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 11,
+		"url" => "https://a0.muscache.com/im/pictures/464ded6d-3de3-455d-bf6f-c2eb57e11030.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 11,
+		"url" => "https://a0.muscache.com/im/pictures/686bd8d8-e35e-41af-af7e-07cce68b46df.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 11,
+		"url" => "https://a0.muscache.com/im/pictures/96240435-bdac-460e-ad3c-5ba1fe89ce1d.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 11,
+		"url" => "https://a0.muscache.com/im/pictures/c7222636-0198-4ae1-a1db-3a26550c06ee.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 12,
+		"url" => "https://a0.muscache.com/im/pictures/95f2eb27-2ba5-4f6f-9517-2a22cb076e27.jpg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 12,
+		"url" => "https://a0.muscache.com/im/pictures/4a677138-6fc2-4cd7-b001-31d4bccd8aae.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 12,
+		"url" => "https://a0.muscache.com/im/pictures/b6e9c352-1151-4745-850e-70868ba7812c.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 12,
+		"url" => "https://a0.muscache.com/im/pictures/66ea7453-c246-4100-88e4-cede8787f6cb.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 12,
+		"url" => "https://a0.muscache.com/im/pictures/d5d80a98-e388-4059-9245-dbfd2ea618b3.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 13,
+		"url" => "https://a0.muscache.com/im/pictures/d816f176-828a-4d70-b8f0-66c4eb1849b1.jpg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 13,
+		"url" => "https://a0.muscache.com/im/pictures/miso/Hosting-19588901/original/1f5ac988-6f26-400e-9fce-36abb7b9c966.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 13,
+		"url" => "https://a0.muscache.com/im/pictures/16e16673-8a8b-4191-901b-6e6987190778.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 13,
+		"url" => "https://a0.muscache.com/im/pictures/6adb0d4d-cedb-4618-a521-64e2d0301236.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 13,
+		"url" => "https://a0.muscache.com/im/pictures/24209873-830f-4ae0-9a6c-e5c0b8cf7380.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 14,
+		"url" => "https://a0.muscache.com/im/pictures/9ff8811f-a337-4e1d-ae1b-6956a3db0d2b.jpg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 14,
+		"url" => "https://a0.muscache.com/im/pictures/bc78eaa8-e340-4e65-9569-7ad91655baef.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 14,
+		"url" => "https://a0.muscache.com/im/pictures/37a41523-cd64-4ad5-bf88-cd23e6d84d31.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 14,
+		"url" => "https://a0.muscache.com/im/pictures/f8629c86-ffab-45bd-86bd-e506438080a6.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 14,
+		"url" => "https://a0.muscache.com/im/pictures/92dca3bd-4014-432f-8d51-04b12e5f5116.jpg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 15,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-22545970/original/dba53791-9db1-42ba-8752-3a6ff475bdaa.jpeg?im_w=1200",
+		"main_image" => true
+	],
+	[
+		"apartment_id" => 15,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-22545970/original/73b78b68-ac8c-4f87-86bb-91a85d1bff9b.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 15,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-22545970/original/5f77c04e-c843-42f2-9525-9e687883df53.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 15,
+		"url" => "ttps://a0.muscache.com/im/pictures/prohost-api/Hosting-22545970/original/4c3c3bfb-ff9c-43b2-93f9-de08b6712765.jpeg?im_w=720",
+		"main_image" => false
+	],
+	[
+		"apartment_id" => 15,
+		"url" => "https://a0.muscache.com/im/pictures/prohost-api/Hosting-22545970/original/9f38cd45-744e-4fb6-92e8-17598ee174e4.jpeg?im_w=720",
+		"main_image" => false
+	],
+];
