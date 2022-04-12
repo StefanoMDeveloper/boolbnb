b4081070fdf17eea3c0340cfd182e90d77c5271b
StefanoMDeveloper <stenmarti02@gmail.com>
2022-04-12 13:00:43 +0200

    Seeder/config appartment

--- database/seeds/ApartmentSeeder.php
+++ database/seeds/ApartmentSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Apartment;
 
 class ApartmentSeeder extends Seeder
 {
@@ -11,6 +13,22 @@ class ApartmentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $apartments = config('apartments');
+        foreach ($apartments as $apartment){
+            $new_apartment = new Apartment();
+            $new_apartment->name = $apartment['name'];
+            $new_apartment->slug = Str::of($apartment['name'])->slug("-");
+            $new_apartment->description = $apartment['description'];
+            $new_apartment->rooms = $apartment['rooms'];
+            $new_apartment->beds = $apartment['beds'];
+            $new_apartment->bathrooms = $apartment['bathrooms'];
+            $new_apartment->square_meters = $apartment['square_meters'];
+            $new_apartment->address = $apartment['address'];
+            $new_apartment->visible = $apartment['visible'];
+            $new_apartment->lat = $apartment['lat'];   
+            $new_apartment->lon = $apartment['lon'];
+            $new_apartment->user_id = rand(1,5);
+            $new_apartment->save();
+        }
     }
 }
