04764a45f550c972d6b4f34b02e67a2ffb8bc4e2
AntonyMorgante <antonymorgante1@gmail.com>
2022-04-12 15:26:26 +0200

    imageseeder

--- database/seeds/ImageSeeder.php
+++ database/seeds/ImageSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Image;
 
 class ImageSeeder extends Seeder
 {
@@ -11,6 +12,13 @@ class ImageSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $images = config('apartments_images');
+        foreach ($images as $image){
+            $new_image = new Image();
+            $new_image->url = $image['url'];
+            $new_image->main_image = $image['main_image'];
+            $new_image->apartment_id = $image['apartment_id'];
+            $new_image->save();
+        }
     }
 }
