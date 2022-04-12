3cc12eccd3b2d9172c1d91840175379503ab54f1
BenedettiGianluca <99gian.bene@gmail.com>
2022-04-12 17:38:36 +0200

    Review

---
+++ config/review.php
@@ -0,0 +1,153 @@
+<?php
+
+return [
+    [
+        "title" => "BEST APARTMENT EVER!!!",
+        "content" => "Appartamento ben curato, in un punto strategico per raggiungere le attrazioni della zona. L'host si è dimostrato attento e premuroso. Consigliatissimo!",
+        "rating" => "5.0",
+        "date" => "2021-08-21",
+        "name" => "Davide",
+        "lastname" => "Gabbamondo",
+        "apartment_id" => "1"
+    ],
+
+    [
+        "title" => "Deluso",
+        "content" => "Mansarda piccola, ideale per soggiorni brevi. Zona di periferia. Mezzi di trasporto lontanissimi. ",
+        "rating" => "2.2",
+        "date" => "2022-03-14",
+        "name" => "Riccardo",
+        "lastname" => "Russocaronte",
+        "apartment_id" => "2"
+    ],
+
+    [
+        "title" => "Appartamento senza suoni",
+        "content" => "La posizione della casa è meravigliosa, a non più di un minuto a piedi dalla piazza. La casa è molto accogliente, ed in questo periodo anche molto molto silenziosa, essendo ad un piano alto.",
+        "rating" => "4.6",
+        "date" => "2020-05-23",
+        "name" => "Mauro",
+        "lastname" => "Maurizio",
+        "apartment_id" => "3"
+    ],
+
+    [
+        "title" => "Ottimo!",
+        "content" => "Ottimo alloggio , ottima posizione , host gentilissimo , preciso e disponibile",
+        "rating" => "3.0",
+        "date" => "2021-09-04",
+        "name" => "Calogero",
+        "lastname" => "Gustapane",
+        "apartment_id" => "4"
+    ],
+
+    [
+        "title" => "Shopping a casa",
+        "content" => "A pochi passi potete trovare negozi, ristoranti, aperitivi e tanta gente con la quale interagire. Se è quello che cercate, siete nel posto giusto.",
+        "rating" => "4.5",
+        "date" => "2021-11-15",
+        "name" => "Marco",
+        "lastname" => "Scozzafava",
+        "apartment_id" => "5"
+    ],
+
+    [
+        "title" => "Ti passano i tappi",
+        "content" => "Dettagli molto curati nonostante la semplicità, si vede che la persona che gestisce il posto ha molta attenzione ai dettagli. Leggermente rumoroso, ma provvisti tappi per orecchie",
+        "rating" => "4.4",
+        "date" => "2021-10-06",
+        "name" => "Alex",
+        "lastname" => "Seccafieno",
+        "apartment_id" => "6"
+    ],
+
+    [
+        "title" => "Soddisfatto",
+        "content" => "Ottimo monolocale pulito, moderno, localizzato in posizione invidiabile.",
+        "rating" => "4.1",
+        "date" => "2021-01-22",
+        "name" => "Matteo",
+        "lastname" => "Buonafede",
+        "apartment_id" => "7"
+    ],
+
+    [
+        "title" => "Troppo arredato",
+        "content" => "L’appartamento non è grandissimo ma c’è tutto, anche di più!",
+        "rating" => "3.2",
+        "date" => "2022-02-13",
+        "name" => "Nazzareno",
+        "lastname" => "Salvia",
+        "apartment_id" => "8"
+    ],
+
+    [
+        "title" => "Brutto come immaginavo",
+        "content" => "La mansarda è piccolina ed ospitale, esattamente come me la immaginavo da annuncio. Ottima per una coppia.",
+        "rating" => "3.8",
+        "date" => "2021-04-18",
+        "name" => "Domenico",
+        "lastname" => "Scaramuccia",
+        "apartment_id" => "9"
+    ],
+
+    [
+        "title" => "Ti ringrazio per l'ospitalità",
+        "content" => "Il palazzo era in restauro e un pò fatiscente ma comodo per viaggi di lavoro. grazie per l'ospitalità.",
+        "rating" => "4.0",
+        "date" => "2021-06-05",
+        "name" => "Enrico",
+        "lastname" => "Colombina",
+        "apartment_id" => "10"
+    ],
+
+    [
+        "title" => "Soddisfatti",
+        "content" => "Casa veramente Carina! Al centro città. Ben attrezzata ed accogliente. Host molto disponibile con noi!",
+        "rating" => "4.4",
+        "date" => "2021-09-19",
+        "name" => "Francesco",
+        "lastname" => "Sanguedolce",
+        "apartment_id" => "11"
+    ],
+
+    [
+        "title" => "Un ottimo servizio",
+        "content" => "Appartamento posizionato in modo eccellente, accoglienza puntuale e cortese, servizi anche migliori di quanto scritto nella descrizione: ad esempio c'era il frigo, anche se non precedentemente indicato. Perfetto per viaggiatori singoli o coppie.",
+        "rating" => "4.9",
+        "date" => "2021-10-27",
+        "name" => "Massimo",
+        "lastname" => "Gongolo",
+        "apartment_id" => "12"
+    ],
+
+    [
+        "title" => "Pienamente soddisfatto",
+        "content" => "L'host é stato preciso e cordiale, l’alloggio ha soddisfatto pienamente le mie aspettative.",
+        "rating" => "5.0",
+        "date" => "2021-04-02",
+        "name" => "Riccardo",
+        "lastname" => "Broccoli",
+        "apartment_id" => "13"
+    ],
+
+    [
+        "title" => "Satisfied",
+        "content" => "The apartment was clean and had everything is needed for someone who plans to stay out most of the day to visit the city.",
+        "rating" => "4.7",
+        "date" => "2021-08-19",
+        "name" => "Erik",
+        "lastname" => "Loughty",
+        "apartment_id" => "14"
+    ],
+
+    [
+        "title" => "Soddisfacente",
+        "content" => "per visitare la città è una location bella nonché perfetta, per posizione, confort.",
+        "rating" => "4.1",
+        "date" => "2022-01-15",
+        "name" => "Paolo",
+        "lastname" => "Boccadifuoco",
+        "apartment_id" => "15"
+    ]
+];
