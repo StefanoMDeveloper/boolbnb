7fc561be14df4cd4d3d2880e66fb63080aaf6c59
StefanoMDeveloper <stenmarti02@gmail.com>
2022-04-14 11:53:34 +0200

    added image in index view and visibility controll

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -8,8 +8,9 @@
         <thead>
             <tr>
                 <th scope="col">#</th>
-                <th scope="col">Host Name</th>
                 <th scope="col">Name</th>
+                <th scope="col">Main Image</th>
+                <th scope="col">Host Name</th>
                 <th scope="col">Slug</th>
                 <th scope="col">Rooms</th>
                 <th scope="col">Beds</th>
@@ -21,32 +22,41 @@
         </thead>
         <tbody>
             @foreach ($apartments as $apartment)
-                <tr>
-                    <th scope="row">{{ $apartment->id }}</td>
-                    <td>{{ $apartment->name }}</td>
-                    <td>
-                        @foreach ($apartment->user_id as $host)
-                            {{ $host->name }} {{ $host->lastname }}
+                @if ($apartment->visible)
+                    <tr>
+                        <th scope="row">{{ $apartment->id }}</td>
+                        <td>{{ $apartment->name }}</td>
+                        @foreach ($images as $image)
+                            @if ($image->main_image)
+                            <td>{{ $image->url }}</td>
+                            @endif
                         @endforeach
-                    </td>
-                    <td>{{ $apartment->slug }}</td>
-                    <td>{{ $apartment->rooms }}</td>
-                    <td>{{ $apartment->beds }}</td>
-                    <td>{{ $apartment->bathrooms }}</td>
-                    <td>{{ $apartment->square_meters }}</td>
-                    <td>{{ $apartment->address }}</td>
-                    <td>
-                        <a href="{{ route('admin.apartments.show', $apartment->id) }}"><button type="button"
-                                class="btn btn-primary">vedi</button></a>
-                        <a href="{{ route('admin.apartments.edit', $apartment->id) }}"><button type="button"
-                                class="btn btn-warning">edit</button></a>
-                        <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="POST">
-                            @csrf
-                            @method("DELETE")
-                            <button type="submit" class="btn btn-danger">delete</button>
-                        </form>
-                    </td>
-                </tr>
+                                {{ $host->name }} {{ $host->lastname }}
+                            @endforeach
+                        <td>
+                            @foreach ($apartment->user_id as $host)
+                                {{ $host->name }} {{ $host->lastname }}
+                            @endforeach
+                        </td>
+                        <td>{{ $apartment->slug }}</td>
+                        <td>{{ $apartment->rooms }}</td>
+                        <td>{{ $apartment->beds }}</td>
+                        <td>{{ $apartment->bathrooms }}</td>
+                        <td>{{ $apartment->square_meters }}</td>
+                        <td>{{ $apartment->address }}</td>
+                        <td>
+                            <a href="{{ route('admin.apartments.show', $apartment->id) }}"><button type="button"
+                                    class="btn btn-primary">vedi</button></a>
+                            <a href="{{ route('admin.apartments.edit', $apartment->id) }}"><button type="button"
+                                    class="btn btn-warning">edit</button></a>
+                            <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="POST">
+                                @csrf
+                                @method("DELETE")
+                                <button type="submit" class="btn btn-danger">delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endif
             @endforeach
 
         </tbody>
