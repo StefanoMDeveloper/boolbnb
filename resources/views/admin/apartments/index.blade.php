8a2accbb2fb81f6db7039f871f223a609abda7b0
StefanoMDeveloper <stenmarti02@gmail.com>
2022-04-14 11:12:26 +0200

    view index

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,5 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>prova</h1>
+<div class="container">
+    <a href="{{ route('admin.apartments.create') }}"><button type="button" class="btn btn-success">aggiungi</button></a>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Host Name</th>
+                <th scope="col">Name</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Rooms</th>
+                <th scope="col">Beds</th>
+                <th scope="col">Bathrooms</th>
+                <th scope="col">Square meters</th>
+                <th scope="col">Address</th>
+                <th scope="col">Buttons</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($apartments as $apartment)
+                <tr>
+                    <th scope="row">{{ $apartment->id }}</td>
+                    <td>{{ $apartment->name }}</td>
+                    <td>
+                        @foreach ($apartment->user_id as $host)
+                            {{ $host->name }} {{ $host->lastname }}
+                        @endforeach
+                    </td>
+                    <td>{{ $apartment->slug }}</td>
+                    <td>{{ $apartment->rooms }}</td>
+                    <td>{{ $apartment->beds }}</td>
+                    <td>{{ $apartment->bathrooms }}</td>
+                    <td>{{ $apartment->square_meters }}</td>
+                    <td>{{ $apartment->address }}</td>
+                    <td>
+                        <a href="{{ route('admin.apartments.show', $apartment->id) }}"><button type="button"
+                                class="btn btn-primary">vedi</button></a>
+                        <a href="{{ route('admin.apartments.edit', $apartment->id) }}"><button type="button"
+                                class="btn btn-warning">edit</button></a>
+                        <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="POST">
+                            @csrf
+                            @method("DELETE")
+                            <button type="submit" class="btn btn-danger">delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+</div>
 @endsection
