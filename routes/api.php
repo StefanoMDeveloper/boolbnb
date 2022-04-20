b1c2e4dcfd182bafd29350f033455902a19dbab1
AntonyMorgante <antonymorgante1@gmail.com>
2022-04-20 15:00:16 +0200

    partial api call

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::namespace('Api')->name('api.')->group(function () {
     Route::get("/apartments", "ApartmentController@index")->name('apartments');
     Route::get("/apartments/{slug}", "ApartmentController@show")->name('apartments.show');
+    Route::get('apartments/filter/{search}' , "ApartmentController@filter")->name('apartments.filter');
     Route::get("/users/{id}", "UserController@show")->name('users.show');
 });
