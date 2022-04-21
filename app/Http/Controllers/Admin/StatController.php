2188929a70b48e5c85fc24672bb46bfc9bfe3357
LOSAFABIO <fabio.losa95@gmail.com>
2022-04-21 12:30:15 +0200

    modificato il controller di stat

--- app/Http/Controllers/Admin/StatController.php
+++ app/Http/Controllers/Admin/StatController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
-use App\Profile;
+use App\Apartment;
 use Illuminate\Support\Facades\Auth;
 
 class StatController extends Controller
@@ -28,7 +28,8 @@ class StatController extends Controller
      */
     public function index()
     {
-        $users = User::all();
-        return view('admin.profile', compact('users'));
+        $user_id = User::all();
+        $apartments = Apartment::all()->where('user_id', $user_id);
+        return view('admin.stats', compact('user_id','apartments'));
     }
 }
