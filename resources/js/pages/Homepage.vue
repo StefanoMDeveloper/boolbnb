8bc6f785b059efd2e9f117d4f6407f72e386970b
StefanoMDeveloper <stenmarti02@gmail.com>
2022-04-22 11:17:52 +0200

    fix-homepage

--- resources/js/pages/Homepage.vue
+++ resources/js/pages/Homepage.vue
@@ -48,8 +48,8 @@
         <div class="container-fluid">
             <div class="ms_citiesContainer d-flex flex-column">
                 <h1 class="mb-2">Destinazioni Più Gettonate</h1>
-                <div class="row ms_citiesContainer d-flex col-12">
-                    <div v-for="(element, index) in cities" :key="index" class="ms_cityCard"  :style="{'background-color':colors[index]}">
+                <div class="row ms_flexContainer d-flex col-12">
+                    <div v-for="(element, index) in cities" :key="index" class="ms_cityCard col"  :style="{'background-color':colors[index]}">
                         <div class="ms_imageContainer">
                             <img :src="require('../../../public/storage/uploads/'+element.image+'.jpg') " alt="">
                         </div>
@@ -66,12 +66,12 @@
         <div class="container-fluid">
             <div class="row ms_experienceContainer col-12">
                 <h1>Scopri le Esperienze Boolbnb</h1>
-                <div class="ms_imgsEsp">
-                    <div class="ms_imgEsp">
+                <div class="ms_imgsEsp row">
+                    <div class="ms_imgEsp col">
                         <img  src="../../../public/storage/uploads/experience2.jpg" alt="">
                         <h1 class="ms_esptext text-white">Esperienze <br> per il tuo viaggio</h1>
                     </div>
-                    <div class="ms_imgEsp">
+                    <div class="ms_imgEsp col">
                         <img  src="../../../public/storage/uploads/experience1.jpg" alt="">
                         <h1 class="ms_esptext text-white">Attività <br> da casa</h1>
                     </div>  
@@ -241,7 +241,8 @@ export default {
     .ms_cityCard{
         width: calc(100% / 4 - 20px);
         border-radius: 20px;
-       
+        padding: 0;
+        margin: 0 15px;
         height: 400px;
         overflow: hidden;
         .ms_imageContainer{
@@ -267,12 +268,12 @@ export default {
         display: flex;
         justify-content: space-between;
         .ms_imgEsp{
-            width: 647px;
-            border-radius: 20px;
+            
             overflow: hidden;
             position: relative;
             img{
                 width: 100%;
+                border-radius: 20px;
             }
             .ms_esptext{
                 position: absolute;
