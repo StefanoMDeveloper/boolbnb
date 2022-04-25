8be364acf0bebe9fa9e56d799f66c3be2c3e0a1a
BenedettiGianluca <99gian.bene@gmail.com>
2022-04-25 18:18:05 +0200

    sponsorizzazioni migliorate

--- resources/views/admin/sponsorize.blade.php
+++ resources/views/admin/sponsorize.blade.php
@@ -1,29 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="dropin-container">
-    <form action="{{route('api.make.payment')}}" method="post" id="payment-form">
-    @csrf
-    @method('POST')
-        @foreach ($sponsorships as $sponsorship)
-          <div class="form-check col-10 pl-4">
-            <input class="form-check-input" type="radio" name="sponsorships[]" id="{{$sponsorship->id}}"
-            value="{{$sponsorship->id}}">
-            <label class="form-check-label" for="{{$sponsorship->id}}">
-                {{$sponsorship->name}}, {{$sponsorship->price}}, {{$sponsorship->duration}} ore
-            </label>  
-          </div>
-        @endforeach
-        <div class="bt-drop-in-wrapper">
-            <div id="bt-dropin"></div>
+
+@foreach ($sponsorships as $sponsorshippo)
+    @if($loop->first)
+        @if(count($apartment->active_sponsorships)<=0)
+        <div id="dropin-container">
+            <form action="{{route('api.make.payment')}}" method="post" id="payment-form">
+            @csrf
+            @method('POST')
+                @foreach ($sponsorships as $sponsorship)
+                  <div class="form-check col-10 pl-4">
+                    <input class="form-check-input" type="radio" name="sponsorships[]" id="{{$sponsorship->id}}"
+                    value="{{$sponsorship->id}}">
+                    <label class="form-check-label" for="{{$sponsorship->id}}">
+                        {{$sponsorship->name}}, {{$sponsorship->price}}, {{$sponsorship->duration}} ore
+                    </label>  
+                  </div>
+                @endforeach
+                <div class="bt-drop-in-wrapper">
+                    <div id="bt-dropin"></div>
+                </div>
+                <input id="apartment" name="apartment" value="{{$apartment->id}}" type="hidden" />
+                <input id="nonce" name="payment_method_nonce" type="hidden" />
+                <button type="submit" class="btn btn-primary">Sponsorizza</button>
+            </form>
         </div>
-        <input id="apartment" name="apartment" value="{{$apartment->id}}" type="hidden" />
-        <input id="nonce" name="payment_method_nonce" type="hidden" />
-        <button type="submit" class="btn btn-primary">Sponsorizza</button>
-    </form>
-</div>
+        @else
+            <div class="container d-flex justify-content-center align-items-center">
+                Hai già una sponsorizzazione attiva!
+            </div>
+        @endif
+    @endif
+@endforeach
+
 <script src="https://js.braintreegateway.com/web/dropin/1.31.2/js/dropin.min.js"></script>
 <script>
+    // $todayDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     var form = document.querySelector('#payment-form');
     var client_token = "{{ $token }}";
     braintree.dropin.create({
