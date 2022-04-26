a8aab50ca3f99b54957a7238b2c950185239339a
LOSAFABIO <fabio.losa95@gmail.com>
2022-04-26 13:04:17 +0200

    btn return-back sponsorize

--- resources/views/admin/sponsorize.blade.php
+++ resources/views/admin/sponsorize.blade.php
@@ -2,38 +2,47 @@
 
 @section('content')
 
-@foreach ($sponsorships as $sponsorshippo)
-    @if($loop->first)
-        @if(count($apartment->active_sponsorships)<=0)
-        <div class="container" id="dropin-container">
-            <form action="{{route('api.make.payment')}}" method="post" id="payment-form">
-            @csrf
-            @method('POST')
-            <h3><strong>Sceglie uno dei seguenti pacchetti promozionali:</strong></h3>
-                @foreach ($sponsorships as $sponsorship)
-                  <div class="form-check col-10 pl-4">
-                    <input class="form-check-input" type="radio" name="sponsorships[]" id="{{$sponsorship->id}}"
-                    value="{{$sponsorship->id}}">
-                    <label class="form-check-label" for="{{$sponsorship->id}}">
-                        {{$sponsorship->name}}, {{$sponsorship->price}}, {{$sponsorship->duration}} ore
-                    </label>  
-                  </div>
-                @endforeach
-                <div class="bt-drop-in-wrapper">
-                    <div id="bt-dropin"></div>
+<div class="container" id="dropin-container">
+    @foreach ($sponsorships as $sponsorshippo)
+        @if($loop->first)
+            @if(count($apartment->active_sponsorships)<=0)
+            
+                <form action="{{route('api.make.payment')}}" method="post" id="payment-form">
+                @csrf
+                @method('POST')
+                <h3><strong>Sceglie uno dei seguenti pacchetti promozionali:</strong></h3>
+                    @foreach ($sponsorships as $sponsorship)
+                    <div class="form-check col-10 pl-4">
+                        <input class="form-check-input" type="radio" name="sponsorships[]" id="{{$sponsorship->id}}"
+                        value="{{$sponsorship->id}}">
+                        <label class="form-check-label" for="{{$sponsorship->id}}">
+                            {{$sponsorship->name}}, {{$sponsorship->price}}, {{$sponsorship->duration}} ore
+                        </label>  
+                    </div>
+                    @endforeach
+                    <div class="bt-drop-in-wrapper">
+                        <div id="bt-dropin"></div>
+                    </div>
+                    <input id="apartment" name="apartment" value="{{$apartment->id}}" type="hidden" />
+                    <input id="nonce" name="payment_method_nonce" type="hidden" />
+                    <button type="submit">Sponsorizza</button>
+                </form>
+            @else
+                <div class="container d-flex justify-content-center align-items-center">
+                    Hai già una sponsorizzazione attiva!
                 </div>
-                <input id="apartment" name="apartment" value="{{$apartment->id}}" type="hidden" />
-                <input id="nonce" name="payment_method_nonce" type="hidden" />
-                <button type="submit">Sponsorizza</button>
-            </form>
-        </div>
-        @else
-            <div class="container d-flex justify-content-center align-items-center">
-                Hai già una sponsorizzazione attiva!
-            </div>
+            @endif
         @endif
-    @endif
-@endforeach
+    @endforeach
+
+    <a href="{{ URL::previous() }}">
+        <button type="button" class="btn backBtn m-1 text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 256 512">
+            <path d="M192 448c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l137.4 137.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448z"/></svg>
+            Torna indietro  
+        </button>
+    </a>
+</div>
 
 <script src="https://js.braintreegateway.com/web/dropin/1.31.2/js/dropin.min.js"></script>
 <script>
