586b6c349f043f9f4652dd689bf947a95f383cc1
AntonyMorgante <antonymorgante1@gmail.com>
2022-04-26 09:26:18 +0200

    messages

--- resources/js/pages/SingleApartment.vue
+++ resources/js/pages/SingleApartment.vue
@@ -4,8 +4,10 @@
             <div class="container" v-show="loading==true">
                 <img src="/storage/loadingPage.gif">
             </div>
-<<<<<<< HEAD
-            <h4>Scrivi un messaggio al proprietario dell&#39;appartamento</h4>
+            <h4 class="mt-4">
+                <span class="icon"><i class="fa-solid fa-message"></i></span>
+                Scrivi un messaggio al proprietario dell&#39;appartamento
+            </h4>            
             <form @submit.prevent="sendMail()">
                 <input type="text" id="name" v-model="formData.name" placeholder="Nome">
                 <input type="text" id="lastname" v-model="formData.lastname" placeholder="Cognome">
@@ -32,56 +34,6 @@
             </button>
 
             <div id="map" style="width: 100%; height: 500px; margin-top:50px;">
-=======
-            <div class="container p-3 singleApartment" v-show="loading==false">
-                <h1>{{apartment.name}}</h1>
-                <p><span class="icon mr-2"><i class="fa-solid fa-location-dot"></i></span>{{apartment.address}}</p>
-                <div class="container containerImages">
-                    <span v-for="image in apartment.images" :key="image.id"><!-- non usare ccs su questo span -->
-                        <img v-if="image.main_image" class="main-immagine" :src="`/storage/${image.url}`">
-                        <img v-else class="other-immagini" :src="`/storage/${image.url}`">
-                    </span>
-
-                </div>
-                <h5>{{apartment.description}}</h5>
-                <p class="py-3"><span class="icon pr-1"><i class="fa-solid fa-person-shelter"></i></span>Stanze: {{apartment.rooms}} • <span class="icon pr-1"><i class="fa-solid fa-bed"></i></span> letti: {{apartment.beds}} • <span class="icon pr-1"><i class="fa-solid fa-toilet"></i></span> bagni: {{apartment.bathrooms}} • <span class="icon pr-1"><i class="fa-solid fa-maximize"></i></span> metri quadrati: {{apartment.square_meters}}</p>
-                <h2>
-                    <span class="icon"><i class="fa-solid fa-bell-concierge"></i></span>
-                    Servizi
-                </h2>
-                <div v-for="service in apartment.services" :key="service.id" class="container">
-                    <p>{{service.name}}</p>
-                </div>
-                <h4 class="mt-4">
-                    <span class="icon"><i class="fa-solid fa-message"></i></span>
-                    Scrivi un messaggio al proprietario dell&#39;appartamento
-                </h4>
-                <form @submit.prevent='sendMail'>
-                    <div v-if="authUser==1">
-                        <label for="email">Ciaone!</label>
-                        <input type="email" id="email" name="email">
-                    </div>
-                    <div class="my-3" v-else>
-                        <label for="email">Inserisci la tua email:</label>
-                        <input type="email" id="email" name="email">
-                    </div>
-                    <textarea class="col-8 form-control" id="message" name="message" placeholder="Inserisci qui il messaggio"></textarea>
-                    <input class="ms_submit" type="submit" value="Submit">
-                </form>
-                <button class="ms_back">
-                <a @click="$router.back()">
-                        Torna indietro  
-                </a>
-                </button>
-
-                <div v-show="messageSent">
-                    Messaggio inviato!
-                </div>
-
-                <div id="map" style="width: 100%; height: 500px; margin-top:50px;">
-
-                </div>
->>>>>>> f6091371d29236bdbb2760e98fbc8ebc28582304
 
             </div>
         </div>
@@ -100,7 +52,6 @@ export default {
             lat: "",
             lon: "",
             loading: true,
-<<<<<<< HEAD
             mapped:false,
             formData: {
                 name:"",
@@ -111,9 +62,6 @@ export default {
                 apartment_id: null
             },
             formErrors:{}
-=======
-            
->>>>>>> f6091371d29236bdbb2760e98fbc8ebc28582304
         }
     },
     components: {
@@ -131,13 +79,6 @@ export default {
             this.loading = false;
         });     
     },
-<<<<<<< HEAD
-    mounted(){
-        this.loading = false;
-    },
-=======
- 
->>>>>>> f6091371d29236bdbb2760e98fbc8ebc28582304
     updated(){
         if(!this.mapped && this.lat!=""){
             let center = [this.lon,this.lat];
