d6418498f412c10dfbca881e7f9d7563e0e8a511
LOSAFABIO <fabio.losa95@gmail.com>
2022-04-27 16:43:32 +0200

    ok sponsorize blade

--- resources/views/admin/sponsorize.blade.php
+++ resources/views/admin/sponsorize.blade.php
@@ -1,44 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 <div class="container" id="dropin-container">
-    @foreach ($sponsorships as $sponsorshippo)
-        @if($loop->first)
-            @if(count($apartment->active_sponsorships)<=0)
-            
-                <form action="{{route('api.make.payment')}}" method="post" id="payment-form">
-                @csrf
-                @method('POST')
-                <h3><strong>Sceglie uno dei seguenti pacchetti promozionali:</strong></h3>
-                    @foreach ($sponsorships as $sponsorship)
-                    <div class="form-check col-10 pl-4">
-                        <input class="form-check-input" type="radio" name="sponsorships[]" id="{{$sponsorship->id}}"
-                        value="{{$sponsorship->id}}">
-                        <label class="form-check-label" for="{{$sponsorship->id}}">
-                            {{$sponsorship->name}}, {{$sponsorship->price}}, {{$sponsorship->duration}} ore
-                        </label>  
-                    </div>
-                    @endforeach
-                    <div class="bt-drop-in-wrapper">
-                        <div id="bt-dropin"></div>
-                    </div>
-                    <input id="apartment" name="apartment" value="{{$apartment->id}}" type="hidden" />
-                    <input id="nonce" name="payment_method_nonce" type="hidden" />
-                    <button type="submit">Sponsorizza</button>
-                </form>
-            @else
-                <div class="container d-flex justify-content-center align-items-center">
-                    Hai già una sponsorizzazione attiva!
-                </div>
-            @endif
-=======
-@dd($apartment->active_sponsorships)
-=======
 
->>>>>>> main
 @foreach ($sponsorships as $sponsorshippo)
     @if($loop->first)
         @if(count($apartment->active_sponsorships)<=0)
@@ -46,35 +12,48 @@
             <form action="{{route('api.make.payment')}}" method="post" id="payment-form">
             @csrf
             @method('POST')
-            <h2><strong>{{$apartment->name}}</strong></h2>
-            <h2><strong>{{$apartment->address}}</strong></h2>
-            <h3 class="m-3"><strong>Scegliere uno dei seguenti pacchetti promozionali:</strong></h3>
-                @foreach ($sponsorships as $sponsorship)
-                <div class="pacchettiSponsor">
-                    <!--input class="form-check-input" type="radio" name="sponsorships[]" id="{{$sponsorship->id}}"
-                    value="{{$sponsorship->id}}"-->
-                    <label class="form-check-label labl" for="{{$sponsorship->id}}">
-                        <input type="radio" name="sponsorships[]" value="{{$sponsorship->id}}" id="{{$sponsorship->id}}"/>
-                        <div>Tipologia: {{$sponsorship->name}},<br>
-                            prezzo: {{$sponsorship->price}},<br>
-                            durata: {{$sponsorship->duration}} ore            
-                        </div>
-                    </label>
+                <div class="container-title-sponsor">
+                    <h1 class="text-center my-3">{{$apartment->name}}</h1>
+                    @forelse ($apartment->images as $image)
+                        @if($image->main_image)
+                            <div class="title-image">
+                                <img src="{{asset( 'storage/'.$image->url )}}" alt="">
+                            </div>
+                        @endif
+                        @empty
+                        Non c'è immagine
+                    @endforelse            
+                    <h3 class="">Scegliere uno dei seguenti pacchetti promozionali:</h3>
                 </div>
-                @endforeach
+                <div class="container-sponsor">
+                    @foreach ($sponsorships as $sponsorship)
+                    <div class="pacchettiSponsor">
+                        <!--input class="form-check-input" type="radio" name="sponsorships[]" id="{{$sponsorship->id}}"
+                        value="{{$sponsorship->id}}"-->
+                        <label class="form-check-label labl" for="{{$sponsorship->id}}">
+                        <input type="radio" name="sponsorships[]" value="{{$sponsorship->id}}" id="{{$sponsorship->id}}"/>
+                        <div class="typeSponsor">
+                            <span>Tipologia: {{$sponsorship->name}}</span>
+                            <span>Prezzo: {{$sponsorship->price}}</span>
+                            <span>Durata: {{$sponsorship->duration}} ore</span>           
+                        </div>
+                        </label>
+                    </div>
+                    @endforeach
+                </div>
                 <div class="bt-drop-in-wrapper">
                     <div id="bt-dropin"></div>
                 </div>
                 <input id="apartment" name="apartment" value="{{$apartment->id}}" type="hidden" />
                 <input id="nonce" name="payment_method_nonce" type="hidden" />
-                <button class="sponsorBtn" type="submit">Sponsorizza</button>
-                <a class="d-inline-block" href="{{route('admin.apartments.index')}}">
+                <a class="d-inline-block mx-2" href="{{route('admin.apartments.index')}}">
                     <button type="button" class="btn btn-primary backBtn">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 256 512">
                         <path d="M192 448c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l137.4 137.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448z"/></svg>
                         Torna indietro
                     </button>
                 </a>
+                <button class="sponsorBtn" type="submit">Sponsorizza</button>
             </form>
         </div>
         @else
@@ -91,9 +70,7 @@
                     <img class="tirchioMegalomane" src="{{asset('/storage/uploads/Zio-Paperone-e-i-soldi.jpg')}}">
                 </div>
             </div>
-<<<<<<< HEAD
->>>>>>> main
-=======
+
             <a href="{{route('admin.apartments.index')}}">
                 <button type="button" class="btn btn-primary backBtn2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 256 512">
@@ -101,17 +78,9 @@
                     Torna indietro
                 </button>
             </a>
->>>>>>> main
+            @endif
         @endif
     @endforeach
-
-    <a href="{{ URL::previous() }}">
-        <button type="button" class="btn backBtn m-1 text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 256 512">
-            <path d="M192 448c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l137.4 137.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448z"/></svg>
-            Torna indietro  
-        </button>
-    </a>
 </div>
 
 <script src="https://js.braintreegateway.com/web/dropin/1.31.2/js/dropin.min.js"></script>
