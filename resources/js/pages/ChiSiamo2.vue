d6e06f2ba461088f513bb2020be22ab27655df7f
StefanoMDeveloper <stenmarti02@gmail.com>
2022-04-27 18:59:14 +0200

    chi siamo finished

---
+++ resources/js/pages/ChiSiamo2.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="container">
+      <div class="us row row-cols-3 justify-content-start">
+          <div class="col">
+            <a href="#/">
+                <button>
+                    <div class="inside-button row">
+                        <i class="fa-brands fa-linkedin linkedin"></i>
+                        <p>Antony <br> Morgante</p>
+                        <i class="fa-solid fa-user-ninja icona"></i>
+                        <!-- chiedere a antony quale vuole -->
+                    </div>
+                </button>
+            </a>
+          </div>
+          <div class="col">
+            <a href="#/">
+                <button>
+                    <div class="inside-button row">
+                        <i class="fa-brands fa-linkedin linkedin"></i>
+                        <p>Fabio <br> Losa</p>
+                        <i class="fa-solid fa-user-secret icona"></i>
+                    </div>
+                </button>
+            </a>
+          </div>
+          <div class="col">
+            <a href="#/">
+                <button>
+                    <div class="inside-button row">
+                        <i class="fa-brands fa-linkedin linkedin"></i>
+                        <p>Ghadeer <br> Shamshoum</p>
+                        <i class="fa-solid fa-user-nurse icona"></i>
+                    </div>
+                </button>
+            </a>
+          </div>
+          <div class="col">
+            <a href="#/">
+                <button>
+                    <div class="inside-button row">
+                        <i class="fa-brands fa-linkedin linkedin"></i>
+                        <p>Gianluca <br> Benedetti</p>
+                        <i class="fa-solid fa-user-doctor icona"></i>
+                    </div>
+                </button>
+            </a>
+          </div>
+          <div class="col">
+            <a href="https://www.linkedin.com/in/stefano-martignoni-922207238/">
+                <button>
+                    <div class="inside-button row">
+                        <i class="fa-brands fa-linkedin linkedin"></i>
+                        <p>Stefano <br> Martignoni</p>
+                        <i class="fa-solid fa-user-astronaut icona"></i>
+                    </div>
+                </button>
+            </a>
+          </div>
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ChiSiamo2",
+}
+</script>
+
+<style scoped lang="scss">
+    .us{
+        margin: 200px 0;
+    }
+
+    button{
+        padding: 5px 10px;
+        margin: 10px;
+        border-radius: 10px;
+        border:0;
+        width: 260px;
+            p{
+                margin-left: 5px;
+                float: left;
+                width: 82px;
+            }
+
+            .linkedin{
+                color: #ff385c;
+                width: 50px;
+                height: 50px;
+                vertical-align:text-bottom;
+                float: left;
+                padding-right: 20px;
+            }
+    }
+
+    .icona{
+        color: #ff385c;
+        width: 50px;
+        height: 50px;
+        vertical-align: middle;
+        float: left;
+        padding-left: 20px;
+    }
+
+    .inside-button{
+        padding: 10px 15px 0 15px;
+    }
+</style>
