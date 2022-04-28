45104bab7273f1fbb9acc42c06c74485a31a8e42
LOSAFABIO <fabio.losa95@gmail.com>
2022-04-28 09:39:53 +0200

    fix btn save -> gestione immagini

--- resources/views/admin/images/edit.blade.php
+++ resources/views/admin/images/edit.blade.php
@@ -49,7 +49,7 @@
         @empty
           Al momento non ci sono immagini.
         @endforelse  
-        <h1>Aggiungi Immagini</h1>
+        <h1>Aggiungi altre immagini</h1>
         <form action="{{route('admin.images.store', $apartment->id)}}" method="POST" enctype="multipart/form-data">
           @csrf
           <div class="form-group col-10 ml-2 mt-3 p-0">
@@ -58,7 +58,7 @@
           </div>
           <div class="d-flex">
             <a href="{{route('admin.apartments.index')}}">
-              <button type="button" class="btn ms_saveBtnD ml-4 mr-2">
+              <button type="button" class="btn saveBtn ml-4 mr-2" style="background-color:#FF385C; color:white">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
                   <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
                 </svg>
